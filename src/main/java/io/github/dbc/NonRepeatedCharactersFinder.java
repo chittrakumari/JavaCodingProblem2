207ce7e24e5c49a637edaa6bf0a3aa6e16c459c5
Chittra Kumari <chittra.kumari_cs.ccv19@gla.ac.in>
2022-05-19 15:26:17 +0530

    Completed the method

--- src/main/java/io/github/dbc/NonRepeatedCharactersFinder.java
+++ src/main/java/io/github/dbc/NonRepeatedCharactersFinder.java
@@ -1,5 +1,7 @@
 package io.github.dbc;
 
+import java.util.Arrays;
+
 public class NonRepeatedCharactersFinder {
     private static final int EXTENDED_ASCII_CODES = 256;
 
@@ -10,6 +12,28 @@ public class NonRepeatedCharactersFinder {
      * @return the first non-repeated character in the string.
      */
     public char findFirstNonRepeatedCharacter(String string) {
-        return Character.MAX_VALUE;
+        if (string == null)
+            throw new IllegalArgumentException();
+        boolean[] visited = new boolean[EXTENDED_ASCII_CODES];
+        int[] arr = new int[EXTENDED_ASCII_CODES];
+        Arrays.fill(arr, Integer.MAX_VALUE);
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < string.length(); i++) {
+            if (!visited[string.charAt(i)]) {
+                arr[string.charAt(i)] = i;
+                visited[string.charAt(i)] = true;
+            } else {
+                arr[string.charAt(i)] = Integer.MAX_VALUE;
+            }
+
+        }
+        for (int i = 0; i < EXTENDED_ASCII_CODES; i++) {
+            if (arr[i] != Integer.MAX_VALUE)
+                min = Math.min(min, arr[i]);
+        }
+        if (min == Integer.MAX_VALUE)
+            return Character.MIN_VALUE;
+        else
+            return string.charAt(min);
     }
 }
